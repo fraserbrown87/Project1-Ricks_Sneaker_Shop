061208a4630fa075fb4ca1174f08f8939b0d3ed9
fraserbrown87 <fraserbrown87@gmail.com>
2017-11-26 18:51:29 +0000

    Initial Commit

---
+++ db/ricks_sneaker_shop.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS brands;
+DROP TABLE IF EXISTS models;
+
+CREATE TABLE brands (
+  id SERIAL4 PRIMARY KEY,
+  brand_name VARCHAR(255)
+);
+
+CREATE TABLE models (
+  id SERIAL4 PRIMARY KEY,
+  model_name VARCHAR(255),
+  brand_id INT4 REFERENCES brands(id),
+  quantity INT2
+);
